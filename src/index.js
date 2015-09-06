a234cc42aa86122028843803b4aa9e442492e562
David Dias <daviddias.p@gmail.com>
2015-09-06 14:39:11 +0100

    it works™

--- src/index.js
+++ src/index.js
@@ -15,19 +15,42 @@ function DRS (mdagStore) {
   self.mdagStore = mdagStore || new MerkleDAGStore()
   self.mapping = {} // {key: [recordSignatureHash]}
 
-  self.get = function (key) {
-  
+  self.get = function (key, callback) {
+    if (!self.mapping[key]) {
+      return callback(null, [])
+    }
+
+    var stillValid = []
+    self.mapping[key].forEach(function (recordSignature) {
+      var recordSignatureMH = multihashing(ipld.marshal(recordSignature), 'sha2-256')
+      var isValid = iprs.validator(recordSignatureMH, self.mdagStore)
+      if (isValid) {
+        stillValid.push(recordSignature)
+      }
+      return
+    })
+    self.mapping[key] = stillValid
+    callback(null, stillValid)
   }
 
   // expects that recordSignature, record and pubkey mdag objs are already put in mdagStore
-  self.put = function (key, recordSignature) {
+  self.put = function (key, recordSignature, callback) {
     // 1. check if valid
     // 2. do the mapping
     // 3. add to other recordStores if any
 
-    var recordSignatureMH = multihashing(ipld.marshal(recordSignature))
-    iprs.validator(recordSignatureMH, self.mdagStore)
+    var recordSignatureMH = multihashing(ipld.marshal(recordSignature), 'sha2-256')
+    var isValid = iprs.validator(recordSignatureMH, self.mdagStore)
 
+    if (!isValid) {
+      return callback(new Error('record is not valid'))
+    }
+
+    if (!self.mapping[key]) {
+      self.mapping[key] = []
+    }
+    self.mapping[key].push(recordSignature)
+
+    callback()
   }
-
 }
